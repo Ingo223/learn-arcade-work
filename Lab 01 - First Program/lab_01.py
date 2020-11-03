315da909fdb1f0ac8ad773ca0d2685e1124629ab
Ingo223 <ingo.staudinger@t-online.de>
2020-11-04 00:15:34 +0100

    more work on lab1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,20 @@
-print("Hi")
+x = "Hello World"
+print(x)
+
+print("1It was a dark and stormy night.")
+print("2Suddenly, a shot rang out")
+
+print("3It was a dark and stormy night."); print("Suddenly, a shot rang out")
+
+print('''4This is a double quotemark:"''')
+print("5Hi! This is a double quote mark: \" We use it for printing.")
+print("6The file is stored in C:\new folder")
+print("7The file is stored in C:\\new folder")
+print("8This \n is \n my \n sample.")
+print("""9You can print
+on multiple
+            lines
+using
+triple
+quotes.""")
+print("Escape codes: \\\\")
